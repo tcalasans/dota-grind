d75a0e4d997d692bcbd1ac6fcc0b7ebf1cb13d04
Tito Calasans <titocalasans@Titos-MacBook-Pro.local>
2026-02-21 15:36:45 -0800

    add minimap data

---
+++ app/resources/creep_wave_timers.js
@@ -0,0 +1,35 @@
+const RADIANT = {
+    "safelane": {
+        "1": 10, // first point
+        "2": 27, // 2nd point
+        "3": 30 // wave meet
+    },
+    "offlane": {
+        "1": 10, // first point
+        "2": 19, // 2nd point
+        "3": 30 // wave meet
+    },
+    "midlane": {
+        "1": 8, // first point
+        "2": 15, // 2nd point
+        "3": 19 // wave meet
+    }
+}
+
+const DIRE = {
+    "safelane": {
+        "1": 10, // first point
+        "2": 27, // 2nd point
+        "3": 30 // wave meet
+    },
+    "offlane": {
+        "1": 10, // first point
+        "2": 19, // 2nd point
+        "3": 30 // wave meet
+    },
+    "midlane": {
+        "1": 8, // first point
+        "2": 15, // 2nd point
+        "3": 19 // wave meet
+    }
+}
